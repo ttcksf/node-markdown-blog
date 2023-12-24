2efc964421b5bedc060119154fd5761ea6a71268
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-12-25 06:47:15 +0900

    init-routing

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,9 @@
 const express = require('express');
 // ルーターオブジェクトを作成(router.get('/', ...) のように、ルートパスと処理内容を指定して、ルーティングを設定)
 const router = express.Router();
+router.get('/new', (req, res) => {
+  // res.send('articles');
+  res.render('articles/new');
+});
 // エクスポート
 module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,12 @@
 
 const express = require('express');
 const nodemon = require('nodemon');
+const mongoose = require('mongoose');
 const articleRouter = require('./routes/articles');
-
+mongoose.connect('mongodb://localhost/blog');
 const app = express();
 // /articlesというスラッグになる
-app.use(articleRouter);
+app.use('/articles', articleRouter);
 
 // ビューエンジンを設定してHTMLなどを使えるようにする
 app.set('view engine', 'ejs');
@@ -18,8 +19,20 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   // res.send('hello');
   // 表示したいファイルのパス
-  res.render('index');
   // 他にもいろんな下層ページをここに書くと冗長になるのでroutesなどディレクトリで管理する
+  // res.render('index');
+  // res.render('index', { text: 'Hello' });
+  const articles = [
+    {
+      title: 'forEachで繰り返し表示する',
+      description: '本文です。',
+    },
+    {
+      title: '変数などを出力するには=をつける',
+      description: '本文です。',
+    },
+  ];
+  res.render('articles/index', { text: articles });
 });
 // サーバーの起動（ルートパスの設定が必要）
 app.listen('5000');
